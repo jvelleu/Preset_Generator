4cc48943813bb2100a4372857aeb20102b7bec61
Jesse Velleu <jvelleu@gmail.com>
2025-01-20 17:34:51 -0500

    Update Assembler

--- Preset_Assembler.py
+++ Preset_Assembler.py
@@ -19,62 +19,53 @@ core_df = pd.read_excel(read_dir,sheet_name="Core Masks",index_col=0)
 
 ## Split profiles, tones, and styles df into main edits & masks
 profiles_dict = {
-    'Main' : profiles_df.iloc[:72],
-    'Adj' : profiles_df.iloc[73:96],
-    'Vert Sky' : profiles_df.iloc[97:120],
-    'Vert Sky Light' : profiles_df.iloc[121:144],
-    'Left Sky' : profiles_df.iloc[145:168],
-    'Left Sky Light' : profiles_df.iloc[169:192],
-    'Right Sky' : profiles_df.iloc[193:216],
-    'Right Sky Light' : profiles_df.iloc[217:240]
+    'Main' : profiles_df.loc[:'Adjustments'].iloc[:-1],
+    'Adj' : profiles_df.loc['Adjustments':'Vertical Sky'].iloc[1:-1],
+    'Vert Sky' : profiles_df.loc['Vertical Sky':'Vertical Sky Light'].iloc[1:-1],
+    'Vert Sky Light' : profiles_df.loc['Vertical Sky Light':'Left Sky'].iloc[1:-1],
+    'Left Sky' : profiles_df.loc['Left Sky':'Left Sky Light'].iloc[1:-1],
+    'Left Sky Light' : profiles_df.loc['Left Sky Light':'Right Sky'].iloc[1:-1],
+    'Right Sky' : profiles_df.loc['Right Sky':'Right Sky Light'].iloc[1:-1],
+    'Right Sky Light' : profiles_df.loc['Right Sky Light':].iloc[1:]
 }
 
 tones_dict = {
-    'Main' : tones_df.iloc[:37],
-    'Adj' : tones_df.iloc[38:61],
-    'Vert Sky' : tones_df.iloc[62:85],
-    'Vert Sky Light' : tones_df.iloc[86:109],
-    'Left Sky' : tones_df.iloc[110:133],
-    'Left Sky Light' : tones_df.iloc[134:157],
-    'Right Sky' : tones_df.iloc[158:181],
-    'Right Sky Light' : tones_df.iloc[182:205]
+    'Main' : tones_df.loc[:'Adjustments'].iloc[:-1],
+    'Adj' : tones_df.loc['Adjustments':'Vertical Sky'].iloc[1:-1],
+    'Vert Sky' : tones_df.loc['Vertical Sky':'Vertical Sky Light'].iloc[1:-1],
+    'Vert Sky Light' : tones_df.loc['Vertical Sky Light':'Left Sky'].iloc[1:-1],
+    'Left Sky' : tones_df.loc['Left Sky':'Left Sky Light'].iloc[1:-1],
+    'Left Sky Light' : tones_df.loc['Left Sky Light':'Right Sky'].iloc[1:-1],
+    'Right Sky' : tones_df.loc['Right Sky':'Right Sky Light'].iloc[1:-1],
+    'Right Sky Light' : tones_df.loc['Right Sky Light':].iloc[1:]
 }
 
 styles_dict = {
-    'Main' : tones_df.iloc[25:30],  # Vignette
-    'Adj' : styles_df.iloc[2:25],
-    'Vert Sky' : styles_df.iloc[31:54],
-    'Vert Sky Light' : styles_df.iloc[55:78],
-    'Left Sky' : styles_df.iloc[79:102],
-    'Left Sky Light' : styles_df.iloc[103:126],
-    'Right Sky' : styles_df.iloc[127:150],
-    'Right Sky Light' : styles_df.iloc[151:174]
+    'Main' : styles_df.loc['Combo Type':'Adjustments'].iloc[1:-1],
+    'Adj' : styles_df.loc['Adjustments':'Vertical Sky'].iloc[1:-1],
+    'Vert Sky' : styles_df.loc['Vertical Sky':'Vertical Sky Light'].iloc[1:-1],
+    'Vert Sky Light' : styles_df.loc['Vertical Sky Light':'Left Sky'].iloc[1:-1],
+    'Left Sky' : styles_df.loc['Left Sky':'Left Sky Light'].iloc[1:-1],
+    'Left Sky Light' : styles_df.loc['Left Sky Light':'Right Sky'].iloc[1:-1],
+    'Right Sky' : styles_df.loc['Right Sky':'Right Sky Light'].iloc[1:-1],
+    'Right Sky Light' : styles_df.loc['Right Sky Light':].iloc[1:]
 }
 
 '''
-tones_adj_df = tones_df.iloc[38:61]
-tones_vertSky_df = tones_df.iloc[62:85]
-tones_vertSkyLight_df = tones_df.iloc[86:109]
-tones_leftSky_df = tones_df.iloc[110:133]
-tones_leftSkyLight_df = tones_df.iloc[134:157]
-tones_rightSky_df = tones_df.iloc[158:181]
-tones_rightSkyLight_df = tones_df.iloc[182:205]
-tones_df = tones_df.iloc[:37]
-styles_adj_df = styles_df.iloc[2:25]
-styles_vertSky_df = styles_df.iloc[31:54]
-styles_vertSkyLight_df = styles_df.iloc[55:78]
-styles_leftSky_df = styles_df.iloc[79:102]
-styles_leftSkyLight_df = styles_df.iloc[103:126]
-styles_rightSky_df = styles_df.iloc[127:150]
-styles_rightSkyLight_df = styles_df.iloc[151:174]
-vignette_df = styles_df.iloc[25:30]
-profiles_adj_df = profiles_df.iloc[73:96]
-profiles_vertSky_df = profiles_df.iloc[97:120]
-profiles_vertSkyLight_df = profiles_df.iloc[121:144]
-profiles_df = profiles_df.iloc[:72]
+#print(styles_dict['Main']['Vignette'])
+#print(tones_dict['Main']['Neutral'].astype(int))
+#print(profiles_dict['Main']['Kodak Porta 400 N'].loc['Red Hue':'Balance'])
+#df = tones_dict['Main']['Neutral'].add(styles_dict['Main']['Vignette'], fill_value=0)
+#df = tones_dict['Main']['Neutral'] + styles_dict['Main']['Vignette']
+#print(df)
+
+#df = profiles_dict['Main']['Kodak Porta 400 N'] + tones_dict['Main']['Neutral'] + styles_dict['Main']['Vignette']
+with pd.option_context('future.no_silent_downcasting', True):
+    df = df.fillna(0).infer_objects(copy=False)
+    #print(df)
 '''
 
-## Create unique combinations of styles according to combo type
+## Find unique combinations of styles according to combo type
 def find_unique_combinations(df):
     valid_combinations = []
     # Iterate through combinations of 1 to 4 columns
@@ -91,12 +82,26 @@ combo_list = find_unique_combinations(combos_df)
 # Add in Base
 combo_list.insert(0,tuple(['Base']))
 
+'''
 ## Assemble master presets dict
 def combine_styles(base_df, style_lst, mask_df, tone_df, profile_df):
     comb_df = base_df
     for style in style_lst:
        comb_df = comb_df + mask_df[style]
     comb_df = comb_df + tone_df + profile_df
+    return comb_df
+'''
+
+## Assemble master presets dict
+def combine_styles(base_df, style_lst, mask_df, tone_df, profile_df):
+    comb_df = base_df
+    print(comb_df.dtypes)
+    for style in style_lst:
+        print(mask_df[style].dtypes)
+        #comb_df = comb_df + mask_df[style]
+        comb_df = comb_df.add(mask_df[style], fill_value=0)
+    print(profile_df.dtypes)
+    comb_df = comb_df.add(tone_df, fill_value=0).add(profile_df, fill_value=0)
     return comb_df
 
 def combine_lightDir(style_name, num_lst, df,df_L=None, df_R=None):
@@ -111,16 +116,29 @@ def combine_lightDir(style_name, num_lst, df,df_L=None, df_R=None):
 preset_dict = {}
 for profile in profiles_df.columns:
     preset_dict[profile] = {}
+
+    # Initialize empty series
+    init_df = profiles_dict['Main'][[profile]]
+    for col in init_df.columns:
+        init_df[col].values[:] = 0
+    init_df = init_df[profile].astype(float)
+
     for tone in tones_df.columns:
         edits_dict = {}
+        prof_tone_df = profiles_dict['Main'][profile]+tones_dict['Main'][tone]
         mask_adj_lst = []
         for i in range(len(combo_list)):
             style_name = " ["+" | ".join(combo_list[i])+"]"     # Style name
             num = str(i+1).zfill(2)                             # Index number
             num_lst = [num, num+"L", num+"R"]                   # Light direction
-            # Base Edits
 
-            # Style Masks
+            # Main Edits
+            print(profiles_dict['Main'][profile])
+            main_df = combine_styles(init_df,combo_list[i], styles_dict['Main'], tones_dict['Main'][tone], profiles_dict['Main'][profile])
+            print(main_df)
+            #edits_dict['Main'] = +styles_dict['Main'][style]
+
+            # Mask Edits
             # Combine Styles
             base_df = styles_dict['Adj']['Base']
             adj_df = combine_styles(base_df,combo_list[i], styles_dict['Adj'], tones_dict['Adj'][tone], profiles_dict['Adj'][profile])
@@ -145,5 +163,5 @@ for profile in profiles_df.columns:
         # Add edits to master preset dict
         preset_dict[profile][tone] = edits_dict
 
-print(preset_dict['Kodak Porta 400 N']['Neutral']['Sky'])
+print(preset_dict['Kodak Porta 400 N']['Neutral']['Adj'])
 
